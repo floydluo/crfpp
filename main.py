e11789d6f0800e6a23b264738b08886ae7c26ef7
floydluo <floydjjluo@gmail.com>
2019-07-31 16:58:27 +0800

    new main file

--- main.py
+++ main.py
@@ -2,41 +2,37 @@ import optparse
 from pprint import pprint
 
 from nlptext.base import BasicObject
-from nlptext.corpus import Corpus
+from nlptext.corpus import Sentence
 
 from crfpp.crftools import load_anno, get_model_name
-from crfpp.train import train
+from crfpp.train import crfpp_train, crfpp_test
+from crfpp.loaddata import get_train_test_valid
 
 
 ########### ResumeNER ###########
-CORPUSPath = 'corpus/ResumeNER/'
-corpusFileIden = '.bmes'
-textType   = 'block'
-Text2SentMethod  = 're'
-Sent2TokenMethod = 'iter'
-TOKENLevel = 'char'
-anno = 'embed'
-annoKW = {}
+from pprint import pprint
+from nlptext.base import BasicObject
 
-BasicObject.INIT(CORPUSPath, corpusFileIden, textType, 
-                 Text2SentMethod, Sent2TokenMethod, TOKENLevel, 
-                 anno, annoKW)
+
+Data_Dir = 'data/ResumeCN/char'
+BasicObject.INIT_FROM_PICKLE(Data_Dir)  
+
 
 CHANNEL_SETTINGS_TEMPLATE = {
     # CTX_IND
     'token':   {'use': True,'Max_Ngram': 1,}, # always the char-level token
-    'basic':   {'use': True,'Max_Ngram': 1, 'end_grain': False},
-    'medical': {'use': True,'Max_Ngram': 1, 'end_grain': False},
-    'radical': {'use': True,'Max_Ngram': 1, 'end_grain': False},
-    'subcomp': {'use': True,'Max_Ngram': 1, 'end_grain': False},
-    'stroke':  {'use': True,'Max_Ngram': 1, 'end_grain': False},
+    'basic':   {'use': False,'Max_Ngram': 1, 'end_grain': False},
+    'medical': {'use': False,'Max_Ngram': 1, 'end_grain': False},
+    'radical': {'use': False,'Max_Ngram': 1, 'end_grain': False},
+    'subcomp': {'use': False,'Max_Ngram': 1, 'end_grain': False},
+    'stroke':  {'use': False,'Max_Ngram': 1, 'end_grain': False},
     # CTX_DEP
-    'pos':     {'use': True, 'tagScheme': 'BIO',},
+    'pos':     {'use': False, 'tagScheme': 'BIOE',},
     # ANNO
-    'annoE':   {'use': True, 'tagScheme': 'BIO',},
+    'annoE':   {'use': True, 'tagScheme': 'BIOE',},
 }
 
-BasicObject.BUILD_GRAIN_UNI_AND_LOOKUP(CHANNEL_SETTINGS_TEMPLATE=CHANNEL_SETTINGS_TEMPLATE)
+BasicObject.BUILD_GV_LKP(CHANNEL_SETTINGS_TEMPLATE=CHANNEL_SETTINGS_TEMPLATE)
 
 
 if __name__ == '__main__':
@@ -53,16 +49,41 @@ if __name__ == '__main__':
     Channel_Settings, tagScheme, labels, tags, tag_size = load_anno(BasicObject, anno_field)
 
     model = get_model_name(BasicObject, Channel_Settings)
-    print(model)
+    print('The model path is:')
+    print('\t', model)
 
     ###########################################
 
-    corpus = Corpus()
-    sents = corpus.Sentences
+    # corpus = Corpus()
+    # sents = corpus.Sentences
 
-    cross_num = 5
-    cross_validation = True
-    seed = 10
-    train(model, sents, Channel_Settings, labels, cross_num, cross_validation = cross_validation, seed = seed)
+    total_sent_num = BasicObject.SENT['length']
+    train_sent_idx, test_sent_idx, valid_sent_idx = get_train_test_valid(total_sent_num, seed = 10)
+    train_sents = [Sentence(i) for i in train_sent_idx]
+    test_sents  = [Sentence(i) for i in test_sent_idx]
+    valid_sents = [Sentence(i) for i in valid_sent_idx]
 
-    ###########################################
+
+    # model = model + '_' + str(cross_idx)
+    para   = crfpp_train(model, train_sents, Channel_Settings)
+
+    log_path = model + '/valid_log.csv'
+    pfm_path = model + '/valid_performance.csv'
+
+    R, Err = crfpp_test(model, valid_sents,  Channel_Settings, labels)
+    Err.to_csv(log_path, index = False, sep = '\t')
+    R.to_csv  (pfm_path, index = True,  sep = '\t')
+
+    print('\nFor Valid:\n')
+    print(R)
+
+
+    log_path = model + '/test_log.csv'
+    pfm_path = model + '/test_performance.csv'
+
+    R, Err = crfpp_test(model, test_sents,  Channel_Settings, labels)
+    Err.to_csv(log_path, index = False, sep = '\t')
+    R.to_csv  (pfm_path, index = True,  sep = '\t')
+
+    print('\nFor Test:\n')
+    print(R)
